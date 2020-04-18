3a2f7d8c9e37086dd62c31804f638d879bd12c7b
K4gur0 <kenshin91cb@gmail.com>
2020-04-18 18:27:54 +0200

    MAJ 3.1 modif header

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,2 @@
 #!/bin/sh
-
 rsync -avz ./ -e "ssh -p 5022"  vinnunbp@world-349.fr.planethoster.net:~/public_html/vinny --include=public/build --include=public/bundles --include=public/.htaccess --include=vendor --exclude-from=.gitignore --exclude=".*"
